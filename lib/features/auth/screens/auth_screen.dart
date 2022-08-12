eefc4b71863a2c21d37a523d137f46d36493bf68
Nyanny <evgenynedosekin@gmail.com>
2022-08-12 16:37:35 +0300

    changed AuthScreen

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,45 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:surf_practice_chat_flutter/features/auth/models/token_dto.dart';
-import 'package:surf_practice_chat_flutter/features/auth/repository/auth_repository.dart';
-import 'package:surf_practice_chat_flutter/features/chat/repository/chat_repository.dart';
-import 'package:surf_practice_chat_flutter/features/chat/screens/chat_screen.dart';
-import 'package:surf_study_jam/surf_study_jam.dart';
+import 'package:surf_practice_chat_flutter/features/auth/screens/widgets/auth_form_widget.dart';
 
 /// Screen for authorization process.
 ///
-/// Contains [IAuthRepository] to do so.
-class AuthScreen extends StatefulWidget {
-  /// Repository for auth implementation.
-  final IAuthRepository authRepository;
 
-  /// Constructor for [AuthScreen].
-  const AuthScreen({
-    required this.authRepository,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<AuthScreen> createState() => _AuthScreenState();
-}
-
-class _AuthScreenState extends State<AuthScreen> {
-  // TODO(task): Implement Auth screen.
-  @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
+class AuthScreen extends StatelessWidget {
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const AuthScreen());
   }
 
-  void _pushToChat(BuildContext context, TokenDto token) {
-    Navigator.push<ChatScreen>(
-      context,
-      MaterialPageRoute(
-        builder: (_) {
-          return ChatScreen(
-            chatRepository: ChatRepository(
-              StudyJamClient().getAuthorizedClient(token.token),
-            ),
-          );
-        },
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: AuthForm(),
+        ),
       ),
     );
   }
