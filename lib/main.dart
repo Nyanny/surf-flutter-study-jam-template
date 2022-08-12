c27691226eaca906f96dcf6a6938e6eb15514048
Nyanny <evgenynedosekin@gmail.com>
2022-08-12 16:36:02 +0300

    changed main.dart to work with bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:surf_practice_chat_flutter/features/auth/repository/auth_repository.dart';
-import 'package:surf_practice_chat_flutter/features/auth/screens/auth_screen.dart';
 import 'package:surf_study_jam/surf_study_jam.dart';
+
+import 'bloc_navigation.dart';
+import 'features/auth/bloc/auth_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-/// App,s main widget.
 class MyApp extends StatelessWidget {
   /// Constructor for [MyApp].
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final AuthRepository authRepository = AuthRepository(StudyJamClient());
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AuthScreen(
-        authRepository: AuthRepository(StudyJamClient()),
+    return RepositoryProvider.value(
+      value: authRepository,
+      child: BlocProvider(
+        create: (_) =>
+            AuthBloc(authRepository: authRepository)..add(AppInitialized()),
+        child: const BlocNavigation(),
       ),
     );
   }
